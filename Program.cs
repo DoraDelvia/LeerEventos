75007c336b207bbc6301e5a5cb567c4528be02f6
DoraDelvia <56735228+DoraDelvia@users.noreply.github.com>
2019-12-26 10:46:24 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LeerEventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Lee archivo 
+            string archivo = System.IO.File.ReadAllText(@"C:\Users\dora_\source\repos\LeerEventos\texto.txt");
+
+           
+            string[] palabras = archivo.Split(',');
+                
+
+
+            ///Imprime lo que contiene el arreglo.
+            foreach (string palabra in palabras)
+            {
+                Console.WriteLine("Evento: {0}", palabra);
+
+
+            }
+
+            Console.Write("");
+            System.Console.ReadKey();
+        }
+    }
+}
